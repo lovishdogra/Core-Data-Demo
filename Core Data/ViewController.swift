3d392cd13931a6a58650e5ae878499caef5d3194
Lovish Dogra <lovishdogra7@gmail.com>
2016-03-22 03:05:25 +0530

    final commit
    Implementation of core data bt saving the data in data model and fetch
    the data by making the request

--- Core Data/ViewController.swift
+++ Core Data/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate //Link the view controller with the appDelegate.
+        let context: NSManagedObjectContext = appDel.managedObjectContext //Link the datamodel with the viewcontroller.
+        let newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context) //Link the variable to save data in data model.
+        newUser.setValue("Lovish", forKey: "username")
+        newUser.setValue("Lov123", forKey: "password")
+        
+        //Save the context in the data model.
+        do{
+            try context.save()
+        } catch{
+            print("Error in saving the data")
+        }
+        
+        //Fetch the data from the data model.
+        let request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        do{
+            let results = try context.executeFetchRequest(request)
+            if results.count > 0{
+                for result in results as! [NSManagedObject]{
+                    print(result.valueForKey("username"))
+                    print(result.valueForKey("password"))
+                }
+            }
+            print(results)
+        } catch{
+            print("Error in fetching the data from the data model.")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
